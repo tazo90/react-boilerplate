4d83ec7d9707047ddeba86447e664195c1ecf343
tazo90 <tazo90@gmail.com>
2021-06-23 08:26:48 +0200

    Add webpack config for dev and prod

---
+++ config/webpack.common.js
@@ -0,0 +1,67 @@
+const path = require('path')
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const PrettierPlugin = require('prettier-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+module.exports = {
+  entry: ['./src/client/index.tsx'],
+
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].[fullhash].bundle.js',
+    publicPath: '/',
+  },
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, '../src/client'),
+      '@resources': path.resolve(__dirname, '../src/resources'),
+    },
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../public'),
+          globOptions: {
+            ignore: ['*.DS_Store', 'favicon.ico', 'template.html'],
+          },
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: './public/template.html',
+      filename: 'index.html',
+    }),
+    new ESLintPlugin({
+      files: ['.', 'src', 'config'],
+      formatter: 'table',
+    }),
+    new PrettierPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|webp|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+}
---
+++ config/webpack.dev.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack')
+
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+  mode: 'development',
+
+  devtool: 'eval-cheap-module-source-map',
+
+  devServer: {
+    historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3333,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              importLoaders: 2,
+              sourceMap: false,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: false,
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                fiber: require('fibers'),
+                includePaths: ['./node_modules'],
+              },
+              sourceMap: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+})
