8affe6191babfb158d9ee69917eaa6ed5e7ab228
tazo90 <tazo90@gmail.com>
2021-06-23 08:17:03 +0200

    Add postcss and tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{js,ts,jsx,tsx}', './public/**/*.html'],
+  },
+  mode: 'jit',
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [],
+}
